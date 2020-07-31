7944ab71897f858936b1ce1c786ee4e3725a442d
phil-lgr <phil@leger.work>
2020-07-31 10:26:07 -0400

    feat(mir-website): cleanup and update netlify build

--- packages/mir-website/public/component---src-pages-index-tsx-fb5b2faa3c58d717f178.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{QeBL:function(e,s,c){"use strict";c.r(s);var a=c("ZKLH"),n=c("ERkP"),o=c.n(n);s.default=()=>{const{gcms:{assets:e}}=a.data;return console.log("1655798060"),console.log(e),console.log(e[0].fileName),o.a.createElement("img",{src:e[0].url})}},ZKLH:function(e){e.exports=JSON.parse('{"data":{"gcms":{"assets":[{"id":"ckd7mcr1s02cy0173xv71ur9a","size":13667,"fileName":"ue.png","url":"https://media.graphcms.com/86nT9xInSfOet96cT6pj"}]}}}')}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-fb5b2faa3c58d717f178.js.map
