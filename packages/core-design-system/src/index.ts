e310d8353912b011643a031480cc0e0c71f2d50e
phil-lgr <phil@leger.work>
2020-08-07 13:48:07 -0400

    feat(core-design-system): init

--- packages/core-design-system/src/index.ts
+++ packages/core-design-system/src/index.ts
@@ -1,6 +1,6 @@
-export * from './colors';
-export * from './typography';
-export * from './modular-scale';
-export * from './typograph';
-export * from './typography';
+// export * from './colors';
+// export * from './typography';
+// export * from './modular-scale';
+// export * from './typograph';
+// export * from './typography';
 export * from './design-system';
---
+++ packages/core-react-ui/src/default.theme.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from 'treat';
+
+export const defautlTheme = createTheme({
+  name: 'mir',
+  colors: {
+    primary: {
+      0: 'red',
+      100: 'red',
+      500: 'red',
+      1000: 'red',
+    },
+  },
+});
