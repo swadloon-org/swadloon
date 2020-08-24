0224f9ef63a95e0d17bdeb92ed190a18ceae5958
phil-lgr <phil@leger.work>
2020-08-24 10:31:43 -0400

    feat(newrade-website): add basic site

--- packages/newrade-website/gatsby-node.ts
+++ packages/newrade-website/gatsby-node.ts
@@ -1,5 +1,27 @@
-export const onCreateWebpackConfig = ({ stage, rules, loaders, plugins, actions }) => {
-  const isDevServer = stage === `develop`;
+import { GatsbyNode } from 'gatsby';
+import { createGatsbyWebpackConfig } from './webpack.config';
+
+/**
+ * Gatsby Node Configuration
+ *
+ * @see https://www.gatsbyjs.com/docs/node-apis/
+ */
+
+export const onCreateBabelConfig: GatsbyNode['onCreateBabelConfig'] = ({ actions }) => {
+  actions.setBabelPlugin({
+    name: 'babel-plugin-treat',
+    options: {},
+  });
+};
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  const isProduction = stage !== `develop`;
   const isSSR = stage.includes(`html`);
-  actions.setWebpackConfig(webpackConfigDev({ isSSR, loaders, plugins }));
+  actions.setWebpackConfig(createGatsbyWebpackConfig({ isProduction, stage, isSSR, loaders, plugins }));
 };
--- packages/newrade-website/src/pages/index.tsx
+++ packages/newrade-website/src/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import Logo from '../illustrations/logo-color.svg';
+
+import styles from './index.module.scss';
+
 export default function Home() {
-  return <div>Hello World !</div>;
+  return (
+    <div className={styles.wrapper}>
+      <Logo />
+    </div>
+  );
 }
