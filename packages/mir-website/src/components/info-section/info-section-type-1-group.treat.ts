74fd313e4c72627c28f444077036d18ccd14af70
phil-lgr <phil@leger.work>
2020-08-24 16:07:22 -0400

    fix(mir-website): fix for info section 1 group

--- packages/mir-website/src/components/info-section/info-section-type-1-group.treat.ts
+++ packages/mir-website/src/components/info-section/info-section-type-1-group.treat.ts
@@ -9,8 +9,8 @@ export const wrapper = style((theme: DesignSystem) => ({
     [`screen and (min-width: ${theme.layout.breakpoints.TABLET_PORTRAIT.valuePx})`]: {
       gridTemplateColumns: '1fr 1fr',
     },
-    [`screen and (min-width: ${theme.layout.breakpoints.DESKTOP_SMALL.valuePx})`]: {
-      maxWidth: '2000px',
-    },
+    // [`screen and (min-width: ${theme.layout.breakpoints.DESKTOP_SMALL.valuePx})`]: {
+    //   maxWidth: '2000px',
+    // },
   },
 }));
