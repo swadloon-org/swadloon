cf353387abd62565efdae16afb77884728c783db
phil-lgr <phil@leger.work>
2020-09-11 17:41:45 -0400

    fix(core-react-ui): fix build command

--- packages/core-react-ui/app/app.tsx
+++ packages/core-react-ui/app/app.tsx
@@ -3,13 +3,16 @@ import { Button } from 'core-react-ui';
 
 import * as styleRefs from './app.treat';
 import { useStyles } from 'react-treat';
+import { ButtonSize } from 'core-design-system';
 
 export function App() {
   const styles = useStyles(styleRefs);
 
   return (
     <div>
-      <Button>hey</Button>
+      <Button size={ButtonSize.large}>hey</Button>
+      <Button size={ButtonSize.medium}>hey</Button>
+      <Button size={ButtonSize.small}>hey</Button>
     </div>
   );
 }
