46a8b4e7c96babaf91e888aec131084ba63be5cc
elliotpellerin <elliot@newrade.ca>
2020-09-29 16:49:22 -0400

    fix(mir-website): job section bug in large desktop fixed with the good operator >=

--- packages/mir-website/src/components/blog-preview/author.tsx
+++ packages/mir-website/src/components/blog-preview/author.tsx
@@ -1,25 +1,25 @@
-import React, { HTMLAttributes } from 'react';
-import { useStyles } from 'react-treat';
-import * as styleRefs from './author.treat';
-import LazyLoad from 'react-lazyload';
+// import React, { HTMLAttributes } from 'react';
+// import { useStyles } from 'react-treat';
+// import * as styleRefs from './author.treat';
+// import LazyLoad from 'react-lazyload';
 
-interface OwnProps {
-  url: any;
-}
+// interface OwnProps {
+//   url: any;
+// }
 
-export const Author: React.FC<OwnProps & { onClick: (e: React.MouseEvent<HTMLDivElement>) => void }> = (props) => {
-  const styles = useStyles(styleRefs);
+// export const Author: React.FC<OwnProps & { onClick: (e: React.MouseEvent<HTMLDivElement>) => void }> = (props) => {
+//   const styles = useStyles(styleRefs);
 
-  return (
-    <div className={styles.wrapper}>
-      <LazyLoad>
-        <div
-          className={styles.imageAuthor}
-          style={{ backgroundImage: `url(https://media.graphcms.com/resize=fit:max,width:300/Jp9hlNc7RViXEuuSNHIw)` }}
-        ></div>
-      </LazyLoad>
-      <div className={styles.fullName}>Émilie Clarke</div>
-      <div className={styles.title}>Directrice du développement des affaires</div>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.wrapper}>
+//       <LazyLoad>
+//         <div
+//           className={styles.imageAuthor}
+//           style={{ backgroundImage: `url(https://media.graphcms.com/resize=fit:max,width:300/Jp9hlNc7RViXEuuSNHIw)` }}
+//         ></div>
+//       </LazyLoad>
+//       <div className={styles.fullName}>Émilie Clarke</div>
+//       <div className={styles.title}>Directrice du développement des affaires</div>
+//     </div>
+//   );
+// };
