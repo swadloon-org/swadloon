822c208ce49328ef88b9106a8f62796a29f8eb98
elliotpellerin <elliot@newrade.ca>
2020-09-29 16:07:34 -0400

    fix(mir-website): fixing layout margin in the blog article

---
+++ packages/mir-website/src/components/blog-preview/author.treat.ts
@@ -0,0 +1,7 @@
+import { DesignSystem, createShadow } from 'core-design-system';
+import { globalStyle, style } from 'treat';
+
+export const wrapper = style((theme: DesignSystem) => ({}));
+export const imageAuthor = style((theme: DesignSystem) => ({}));
+export const fullName = style((theme: DesignSystem) => ({}));
+export const title = style((theme: DesignSystem) => ({}));
