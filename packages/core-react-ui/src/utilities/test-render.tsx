7867cf1a22c4d34b09fc6121c0c5c8bcec063d31
phil-lgr <phil@leger.work>
2020-10-12 15:46:24 -0400

    fix(core-react-ui): fix test import

--- packages/core-react-ui/src/utilities/test-render.tsx
+++ packages/core-react-ui/src/utilities/test-render.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { TreatProvider } from 'react-treat/lib/types';
+import { TreatProvider } from 'react-treat';
 
 export function renderTest(Component: any) {
   return render(<TreatProvider theme={'light'}>{Component}</TreatProvider>);
