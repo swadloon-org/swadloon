e396fc38d9d1a681b9dc169321146b6eb20331b8
elliotpellerin <elliot@newrade.ca>
2020-10-22 19:14:29 -0400

    feat(contenful): adding the contenful migration

---
+++ packages/core-contentful-lib/src/doggo.ts
@@ -0,0 +1,10 @@
+import Migration from 'contentful-migration';
+
+export default function (migration: Migration) {
+  const dog = migration.createContentType('dog', {
+    name: 'Dog',
+  });
+
+  const name = dog.createField('name');
+  name.name('Name').type('Symbol').required(true).validations([]);
+}
--- packages/core-contentful-lib/src/index.ts
+++ packages/core-contentful-lib/src/index.ts
@@ -5,9 +5,9 @@ import { runMigration } from 'contentful-migration';
  */
 
 const options = {
-  filePath: '<migration-file-path>',
-  spaceId: '<space-id>',
-  accessToken: '<access-token>',
+  filePath: '/Users/elliot/Documents/nrRepo/newrade/packages/core-contentful-lib/src/doggo.ts',
+  spaceId: process.env.CONTENTFUL_ACCESS_SPACEID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 };
 
 runMigration(options)
