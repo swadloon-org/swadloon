4d372d2baaef8eb63093b017be0a1b83864ebd4b
phil-lgr <phil@leger.work>
2020-11-01 22:32:59 -0500

    fix(mir-website): add page query

--- packages/mir-website/src/layouts/page.layout.tsx
+++ packages/mir-website/src/layouts/page.layout.tsx
@@ -1,24 +1,20 @@
+import { GatsbyPageContext } from '@newrade/core-gatsby-config';
 import {
   getMetaBasicTags,
   getMetadataOpenGraphWebsiteTags,
   getMetadataTwitterTags,
   OPEN_GRAPH_TYPE,
 } from '@newrade/core-react-ui-old';
-import Helmet from 'react-helmet';
 import { graphql, PageProps } from 'gatsby';
-import React, { useState } from 'react';
-import { TreatProvider, useStyles } from 'react-treat';
-import { Footer } from '../components/footer';
-import { NavBar } from '../components/nav-bar';
-import { SideBar } from '../components/side-bar';
+import React from 'react';
+import Helmet from 'react-helmet';
+import { TreatProvider } from 'react-treat';
+import { PageQuery } from '../../types/graphql-types';
 import { ViewportProvider } from '../context/viewport.context';
 import { light } from '../design-system/themes.treat';
 import { viewportContext } from '../hooks/use-viewport.hook';
 import '../styles/font-faces.styles.css';
-import { Index } from '../templates/index-page.template';
-import * as styleRefs from './page.treat';
-import { GatsbyPageContext } from '@newrade/core-gatsby-config';
-import { PageQuery } from '../../types/graphql-types';
+import { LayoutComponent } from './LayoutComponent';
 
 export type ProjectPageProps = PageProps<PageQuery, GatsbyPageContext>;
 
@@ -28,20 +24,77 @@ export const pageQuery = graphql`
       ...SiteMetadata
     }
     contentfulCompanyInfo {
+      companyName
+      linkedinPageURL
+      facebookPageURL
+      instagramPageURL
+      twitterPageURL
       logo {
         file {
           url
         }
-        fluid {
-          srcSet
+      }
+      logoFooter {
+        file {
+          url
         }
       }
-      twitterPageURL
-      id
+      metadataTwitterSite
+      metadataTwitterCreator
+      metadataSiteName
     }
-    contentfulPage(id: { eq: $pageId }) {
+    contentfulPage(id: { eq: $pageId }, node_locale: { eq: "fr-CA" }) {
       id
       name
+      description {
+        childMdx {
+          body
+        }
+      }
+      title
+      route
+      bannerTitle
+      bannerSubTitle
+      bannerImages {
+        name
+        medias {
+          file {
+            url
+          }
+        }
+      }
+      sections {
+        id
+        name
+        description {
+          childMdx {
+            body
+          }
+        }
+        title
+        titleHighlight
+        titleTab
+        type {
+          id
+          name
+        }
+        illustration {
+          id
+          title
+          description
+          file {
+            url
+          }
+        }
+        imagePosition
+        infoTiles {
+          title
+        }
+        processStep {
+          id
+          title
+        }
+      }
     }
 
     # allContentfulCompanyInfo(filter: { name: { eq: "Home" }, node_locale: { eq: "fr-CA" } }) {
@@ -58,22 +111,6 @@ export const pageQuery = graphql`
     # }
   }
 `;
-
-// const IndexPage: React.FC<PageProps> = (props) => {
-//   return (
-//     <PageLayout
-//       location={props.location}
-//       logoURL={props.data.gcms.companyInfos[0].logo?.url}
-//       linkedinPageURL={props.data.gcms.companyInfos[0].linkedinPageUrl}
-//       facebookPageURL={props.data.gcms.companyInfos[0].facebookPageUrl}
-//       instagramPageURL={props.data.gcms.companyInfos[0].instagramPageUrl}
-//       twitterPageURL={props.data.gcms.companyInfos[0].twitterPageUrl}
-//       pages={props.data.gcms.routes}
-//     >
-//       <Index {...props} />
-//     </PageLayout>
-//   );
-// };
 
 export type NavigationProps = {
   location?: Location;
@@ -93,49 +130,25 @@ export const PageLayout: React.FC<ProjectPageProps> = (props) => {
           {getMetaBasicTags()}
           {getMetadataOpenGraphWebsiteTags({
             type: OPEN_GRAPH_TYPE.WEBSITE,
-            title: `${props.data.gcms.pages[0]?.title}`,
-            url: `${props.data.site?.siteMetadata?.siteUrl}${props.data.gcms.pages[0]?.route}`,
-            description: `${props.data.gcms.pages[0]?.description}`,
-            image: `${props.data.gcms.pages[0]?.bannerImages[0]?.url}`,
-            site_name: `${props.data.gcms.companyInfos[0].metadataSiteName}`,
+            title: `${props.data.contentfulPage.title}`,
+            url: `${props.data.site?.siteMetadata?.siteUrl}${props.data.contentfulPage?.route}`,
+            description: `${props.data.contentfulPage?.description}`,
+            image: `${props.data.contentfulPage?.bannerImages[0]?.url}`,
+            site_name: `${props.data.contentfulCompanyInfo.metadataSiteName}`,
             lang: 'fr',
             locale: 'fr_CA',
             localeAlternate: 'en_CA',
           })}
           {getMetadataTwitterTags({
-            card: 'summary',
-            creator: `${props.data.gcms.companyInfos[0].metadataTwitterCreator}`,
-            site: `${props.data.gcms.companyInfos[0].metadataTwitter}`,
+            card: 'summary_large_image',
+            image: `${props.data.contentfulPage?.bannerImages[0]?.url}`,
+            creator: `${props.data.contentfulCompanyInfo.metadataTwitterCreator}`,
+            site: `${props.data.contentfulCompanyInfo.metadataTwitterSite}`,
           })}
         </Helmet>
         <LayoutComponent {...props} />
       </TreatProvider>
     </ViewportProvider>
-  );
-};
-
-const LayoutComponent: React.FC<ProjectPageProps> = (props) => {
-  const styles = useStyles(styleRefs);
-  const [sideMenuState, setSideMenuState] = useState<'opened' | 'closed'>('closed');
-
-  function onOpenSideMenu() {
-    setSideMenuState(sideMenuState === 'opened' ? 'closed' : 'opened');
-  }
-
-  return (
-    <>
-      <SideBar
-        className={`${styles.sidebar}`}
-        state={sideMenuState}
-        onOpenSideMenu={onOpenSideMenu}
-        {...props}
-      ></SideBar>
-      <div className={`${sideMenuState === 'opened' ? styles.open : styles.close}`}>
-        <NavBar onOpenSideMenu={onOpenSideMenu} {...props}></NavBar>
-        {props.children}
-        <Footer></Footer>
-      </div>
-    </>
   );
 };
 
