784ccca4bc301191ec1f8545700a68fe38f85a4c
github-username <github-email>
2020-11-10 14:39:34 -0500

    feat(core-gatsby-starter): wip sur les components html / markdown

--- packages/core-react-ui/src/global/default-css.treat.ts
+++ packages/core-react-ui/src/global/default-css.treat.ts
@@ -1,9 +1,15 @@
-import { globalStyle } from 'treat';
+import { DesignSystem } from '@newrade/core-design-system';
+import { globalStyle, style } from 'treat';
+import { Theme } from '../design-system/css-design-system';
+
+export const wrapper = style((theme: DesignSystem) => ({
+  display: 'inherit',
+}));
 
 /**
  * Styles for kbd element
  */
-globalStyle(`kbd`, {
+globalStyle(`${wrapper} kbd`, ({ cssTheme, theme }: Theme) => ({
   display: 'inline-block',
   margin: '0 .1em',
   padding: '.1em .6em',
@@ -11,10 +17,19 @@ globalStyle(`kbd`, {
   fontSize: '11px',
   lineHeight: '1.5',
   color: '#424242',
+  // color: cssTheme.colors.grey[1000],
   textShadow: '0 1px 0 #fff',
   backgroundColor: '#f3f3f3',
   border: '1px solid grey',
   borderRadius: '3px',
   boxShadow: '0 1px 1px rgba(12,13,14,0.15), inset 0 1px 0 0 #fff',
   overflowWrap: 'break-word',
-});
+}));
+
+/**
+ * Default styles for table elements
+ */
+globalStyle(`${wrapper} table`, ({ cssTheme, theme }: Theme) => ({
+  display: 'inline-block',
+  borderColor: '#424242',
+}));
