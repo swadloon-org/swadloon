9fcba5b7338c2be77f355d9f8afe5c2ae83b5a0c
elliotpellerin <elliot@newrade.ca>
2020-11-13 12:33:23 -0500

    feat(layout): adding treat style

--- packages/core-gatsby-starter/src/pages/index.treat.ts
+++ packages/core-gatsby-starter/src/pages/index.treat.ts
@@ -1,6 +1,29 @@
 import { DesignSystem } from '@newrade/core-design-system';
-import { style } from 'treat';
+import { globalStyle, style } from 'treat';
 
 export const wrapper = style((theme: DesignSystem) => ({
   boxSizing: 'border-box',
+}));
+
+globalStyle(`${wrapper}  table`, (theme: DesignSystem) => ({
+  minWidth: '100%',
+  borderCollapse: 'collapse',
+}));
+globalStyle(`${wrapper}  thead`, (theme: DesignSystem) => ({
+  display: 'table-header-group',
+  verticalAlign: 'middle',
+}));
+globalStyle(`${wrapper}  tbody`, (theme: DesignSystem) => ({
+  display: 'table-row-group',
+  verticalAlign: 'middle',
+}));
+globalStyle(`${wrapper}  tr`, (theme: DesignSystem) => ({
+  display: 'table-row',
+  verticalAlign: 'inherit',
+}));
+globalStyle(`${wrapper}  th`, (theme: DesignSystem) => ({
+  letterSpacing: '0.05em',
+  textTransform: 'uppercase',
+  color: 'rgba(107, 114, 128, var(--text-opacity))',
+  textAlign: 'left',
 }));
