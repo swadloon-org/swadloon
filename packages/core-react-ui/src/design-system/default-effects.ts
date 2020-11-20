3e78889f6ad1d26dd9fd42ebd3f1b27df231ab14
elliotpellerin <elliot@newrade.ca>
2020-11-19 19:41:00 -0500

    feat(core-react-ui): adding the default shadows

---
+++ packages/core-react-ui/src/design-system/default-effects.ts
@@ -0,0 +1,7 @@
+import { Effects } from '@newrade/core-design-system';
+
+export const defaultShadowsShadows: Effects['shadows'] = {
+  light: { blur: 0, spread: 0, offsetX: 1, offsetY: 0, color: { h: 200, s: 50, l: 50 } },
+  medium: { blur: 1, spread: 1, offsetX: 1, offsetY: 1, color: { h: 200, s: 50, l: 50 } },
+  heavy: { blur: 1, spread: 1, offsetX: 1, offsetY: 1, color: { h: 200, s: 50, l: 50 } },
+};
--- packages/core-react-ui/src/utilities/shadows.ts
+++ packages/core-react-ui/src/utilities/shadows.ts
@@ -1,15 +1,19 @@
 import { BoxShadow } from '@newrade/core-design-system';
+import { defaultTheme } from 'src/design-system/default-theme';
 import { getCSSColor } from './colors.utilities';
+import { defaultShadowsShadows } from '../design-system/default-effects';
 
 export function getCSSShadow(options: BoxShadow): string {
   return `${options.offsetX}px ${options.offsetY}px ${options.blur}px ${options.spread}px ${getCSSColor(
     options.color
       ? options.color
       : {
-          h: 0,
+          h: 100,
           s: 100,
           l: 100,
           a: 20,
         }
   )}`;
 }
+
+export function createDefaultShadows() {}
