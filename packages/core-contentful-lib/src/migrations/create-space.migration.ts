694478672ac244ebe0c750567775a59f101665a4
elliotpellerin <elliot@newrade.ca>
2020-12-03 12:00:51 -0500

    fix(migration): fixing bad import

--- packages/core-contentful-lib/src/migrations/create-space.migration.ts
+++ packages/core-contentful-lib/src/migrations/create-space.migration.ts
@@ -1,4 +1,7 @@
 import * as Migration from 'contentful-migration';
+import { createLink } from '../content-types-default/basic/link';
+import { createMediaCollection } from '../content-types-default/basic/media-collection';
+import { createTile } from '../content-types-default/basic/tile';
 import { createBlogAuthor } from '../content-types-default/blog/blog-author';
 import { createBlogPost } from '../content-types-default/blog/blog-post';
 import { createPage } from '../content-types-default/page/page';
@@ -6,9 +9,6 @@ import { createSection } from '../content-types-default/section/section';
 import { createSectionType } from '../content-types-default/section/section-type';
 import { createCompanyAdress } from '../content-types-default/static/company-address';
 import { createCompanyInfo } from '../content-types-default/static/company-info';
-import { createLink } from '../content-types-default/ui/link';
-import { createMediaCollection } from '../content-types-default/ui/media-collection';
-import { createTile } from '../content-types-default/ui/tile';
 
 const program: Migration.MigrationFunction = function IndexModel(migration) {
   /**
