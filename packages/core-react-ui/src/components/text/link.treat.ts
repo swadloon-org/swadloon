364cf535802a1f852f6c836bdc07d1d2b291e93c
elliotpellerin <elliot.pellerin@outlook.com>
2021-01-05 21:10:50 -0500

    feat(link-style): adding new enum of link-style

--- packages/core-react-ui/src/components/text/link.treat.ts
+++ packages/core-react-ui/src/components/text/link.treat.ts
@@ -1,8 +1,36 @@
+import { LINK_STYLE, TEXT_LEVEL, TEXT_STYLE } from '@newrade/core-design-system';
 import { style } from 'treat';
 import { Theme } from '../../design-system/css-design-system';
+import { getCSSTextStyles } from '../../utilities/text.utilities';
 
-export const styles = {
-  wrapper: style(({ theme, cssTheme }: Theme) => ({})),
+export const styles: { [key in TEXT_STYLE]: string } &
+  { [key in LINK_STYLE]: string } &
+  { [key in TEXT_LEVEL]: string } = {
   normal: style(({ theme, cssTheme }: Theme) => ({})),
   underLine: style(({ theme, cssTheme }: Theme) => ({})),
+  italic: style(({ theme, cssTheme }: Theme) => ({
+    ...getCSSTextStyles(cssTheme.typography.labels.styles.italic),
+  })),
+  bold: style(({ theme, cssTheme }: Theme) => ({
+    ...getCSSTextStyles(cssTheme.typography.labels.styles.bold),
+  })),
+  uppercase: style(({ theme, cssTheme }: Theme) => ({
+    ...getCSSTextStyles(cssTheme.typography.labels.styles.uppercase),
+  })),
+  boldUppercase: style(({ theme, cssTheme }: Theme) => ({
+    ...getCSSTextStyles(cssTheme.typography.labels.styles.boldUppercase),
+  })),
+  italicBold: style(({ theme, cssTheme }: Theme) => ({
+    ...getCSSTextStyles(cssTheme.typography.labels.styles.italicBold),
+  })),
+
+  primary: style(({ theme, cssTheme }: Theme) => ({
+    color: cssTheme.colors.colorIntents.primaryText,
+  })),
+  secondary: style(({ theme, cssTheme }: Theme) => ({
+    color: cssTheme.colors.colorIntents.secondaryText,
+  })),
+  tertiary: style(({ theme, cssTheme }: Theme) => ({
+    color: cssTheme.colors.colorIntents.tertiaryText,
+  })),
 };
--- packages/core-react-ui/src/components/text/link.tsx
+++ packages/core-react-ui/src/components/text/link.tsx
@@ -1,33 +1,32 @@
-import { PARAGRAPH_SIZE, TEXT_STYLE } from '@newrade/core-design-system';
+import { PARAGRAPH_SIZE, TEXT_STYLE, TEXT_LEVEL, LINK_STYLE } from '@newrade/core-design-system';
 import React, { HTMLAttributes, useState, AnchorHTMLAttributes } from 'react';
 import { useStyles } from 'react-treat';
 import { CommonComponentProps } from '../../props/component-common.props';
 import * as stylesRef from './link.treat';
 
-export enum LINK_STYLE {
-  normal = 'normal',
-  underLine = 'underLine',
-}
-
 type Props = CommonComponentProps &
   AnchorHTMLAttributes<any> & {
-    variantStyle?: LINK_STYLE;
+    variantLinkStyle?: LINK_STYLE;
     icon?: string | undefined;
-    variantState?: 'normal' | 'disabled' | 'hover' | 'focused';
+    variantStyle?: TEXT_STYLE;
+    variantLevel?: TEXT_LEVEL;
   };
 
 const defaultProps: Props = {
-  variantStyle: LINK_STYLE.normal,
+  variantLinkStyle: LINK_STYLE.normal,
   children: 'Link',
-  variantState: 'normal',
 };
 
-export const Link: React.FC<Props> = React.memo(({ variantStyle, variantState, className, ...props }) => {
-  const { styles: styles } = useStyles(stylesRef);
+export const Link: React.FC<Props> = React.memo(
+  ({ variantLinkStyle, icon, variantStyle, variantLevel, className, ...props }) => {
+    const { styles: styles } = useStyles(stylesRef);
 
-  const type = 'a';
-  return React.createElement(type, {
-    className: `${className || ''} ${styles[variantStyle ? variantStyle : (defaultProps.variantStyle as LINK_STYLE)]}`,
-    ...props,
-  });
-});
+    const type = 'a';
+    return React.createElement(type, {
+      className: `${className || ''} ${
+        styles[variantLinkStyle ? variantLinkStyle : (defaultProps.variantLinkStyle as LINK_STYLE)]
+      }`,
+      ...props,
+    });
+  }
+);
