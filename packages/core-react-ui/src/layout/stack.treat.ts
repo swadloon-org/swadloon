b7b7ef033854f96adcc19576158ed53271cdf1ee
elliotpellerin <elliot.pellerin@outlook.com>
2021-01-05 19:56:31 -0500

    feat(footer): correct view of the footer

--- packages/core-react-ui/src/layout/stack.treat.ts
+++ packages/core-react-ui/src/layout/stack.treat.ts
@@ -1,7 +1,14 @@
-import { style } from 'treat';
+import { styleMap } from 'treat';
 import { Theme } from '../design-system/css-design-system';
 
-export const wrapper = style(({ cssTheme, theme }: Theme) => ({
-  display: 'grid',
-  gridTemplateColumns: '1fr',
+export const styles = styleMap(({ cssTheme, theme }: Theme) => ({
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gap: `var(--mobileGap)` as string,
+    '@media': {
+      [`(min-width: 700px)`]: { gap: `var(--tabletGap)` as string },
+      ['(min-width: 1200px)']: { gap: `var(--desktopGap)` as string },
+    },
+  },
 }));
