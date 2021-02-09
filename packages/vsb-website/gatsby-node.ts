bbc4ae8eb3d999be91e57fa741d5325096ff1816
elliotpellerin <elliot.pellerin@outlook.com>
2021-02-09 15:59:42 -0500

    feat(postbuild-netlify-functions): adding the postbuild in gatsby-node for netlify

--- packages/vsb-website/gatsby-node.ts
+++ packages/vsb-website/gatsby-node.ts
@@ -1,9 +1,11 @@
+import child_process from 'child_process';
 import { GatsbyNode } from 'gatsby';
 /**
  * Gatsby Node Configuration
  *
  * @see https://www.gatsbyjs.com/docs/node-apis/
  */
+import util from 'util';
 
 export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions;
@@ -32,4 +34,18 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
    * /spermogramme.php
    * /joindre.php
    */
+};
+
+const exec = util.promisify(child_process.exec);
+
+export const onPostBuild = async (gatsbyNodeHelpers: any) => {
+  const { reporter } = gatsbyNodeHelpers;
+
+  const reportOut = (report: any) => {
+    const { stderr, stdout } = report;
+    if (stderr) reporter.error(stderr);
+    if (stdout) reporter.info(stdout);
+  };
+  console.log('lambda test');
+  reportOut(await exec('yarn lambda'));
 };
