037d9bf3721c8e40ecbf0121bcf6a0890d9f69ab
elliotpellerin <elliot.pellerin@outlook.com>
2021-02-25 15:38:32 -0500

    Pushing current fix without verification

--- packages/core-cli/src/commands/figma-sync.ts
+++ packages/core-cli/src/commands/figma-sync.ts
@@ -1,11 +1,8 @@
 import { extract } from '@newrade/core-figma-extractor/src';
 import { loadDotEnv } from '@newrade/core-utils';
 import { Command, flags } from '@oclif/command';
-import debug from 'debug';
 // import packageJson from '../../package.json'; // TODO: check if possible to load local package json
 import { Env, ENV } from '../types/dot-env';
-
-const log = debug('newrade:core-cli');
 
 const env = loadDotEnv<ENV>({
   schema: Env,
