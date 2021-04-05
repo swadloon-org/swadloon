9039f1c9fdef9948d90553685ee079c66ddc554a
phil-lgr <phil@leger.work>
2021-04-05 14:43:54 -0400

    feat(vsb-api): add health-check

--- packages/vsb-api/src/health-check.ts
+++ packages/vsb-api/src/health-check.ts
@@ -1,6 +1,11 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
+import debug from 'debug';
+
+const log = debug('newrade').extend('vsb-api').extend('health-check');
+
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name = 'World' } = request.query;
+  log(`received health check from ip:${request.headers.origin}`);
   response.status(200).send(`Hello ${name}!`);
 };
