4c69723fb1027925b057b53c58664570931b78fd
phil-lgr <phil@leger.work>
2021-05-18 10:27:22 -0400

    fix(core-react-ui): fix error

--- packages/core-react-ui/src/text/blockquote.treat.ts
+++ packages/core-react-ui/src/text/blockquote.treat.ts
@@ -1,13 +1,12 @@
 import { style } from 'treat';
 import { Theme } from '../design-system';
-import { getCSSColor } from '../utilities/colors.utilities';
 
 export const styles = {
   normal: style(({ theme, cssTheme }: Theme) => ({
     userSelect: 'text',
     color: cssTheme.colors.colorIntents.primaryText,
-    backgroundColor: getCSSColor({ ...theme.colors.colors.primary[100], a: 25 }),
-    borderLeft: `8px solid ${cssTheme.colors.colors.primary[500]}`,
-    padding: `20px 16px 20px 25px`,
+    backgroundColor: cssTheme.colors.colorIntents.background1,
+    border: `1px solid ${cssTheme.colors.colors.grey[50]}`,
+    padding: cssTheme.sizing.var.x3,
   })),
 };
