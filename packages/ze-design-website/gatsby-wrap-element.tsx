78861e433d48642648cd8705008f0d21359f96a4
phil-lgr <phil@leger.work>
2021-07-01 14:38:01 -0400

    fix(ze-design-system): update logos

--- packages/ze-design-website/gatsby-wrap-element.tsx
+++ packages/ze-design-website/gatsby-wrap-element.tsx
@@ -1,8 +1,6 @@
 import { GatsbyCommonPageContext } from '@newrade/core-gatsby-config';
 import { LazyLayoutDesignSystem, LazyLayoutDocs } from '@newrade/core-gatsby-ui/src';
 import { cssTheme, theme } from '@newrade/ze-design-system';
-import LogoSymbol from '@newrade/ze-design-system/lib/assets/logo-symbol.svg';
-import Logo from '@newrade/ze-design-system/lib/assets/logo.svg';
 import { PageProps, WrapPageElementBrowserArgs } from 'gatsby';
 import React from 'react';
 import ProvidersDocs from './src/context/providers-docs';
@@ -28,22 +26,14 @@ export const WrapElement: React.FC<WrapPageElementBrowserArgs> = ({ element, pro
     case 'docs': {
       return (
         <ProvidersDocs>
-          <LazyLayoutDocs DesktopSvgLogo={<Logo />} MobileSvgLogo={<LogoSymbol />} {...pageProps}>
-            {element}
-          </LazyLayoutDocs>
+          <LazyLayoutDocs {...pageProps}>{element}</LazyLayoutDocs>
         </ProvidersDocs>
       );
     }
     case 'designSystem': {
       return (
         <ProvidersDocs>
-          <LazyLayoutDesignSystem
-            treatThemeRef={light}
-            theme={{ theme, cssTheme }}
-            DesktopSvgLogo={<Logo />}
-            MobileSvgLogo={<LogoSymbol />}
-            {...pageProps}
-          >
+          <LazyLayoutDesignSystem treatThemeRef={light} theme={{ theme, cssTheme }} {...pageProps}>
             {element}
           </LazyLayoutDesignSystem>
         </ProvidersDocs>
