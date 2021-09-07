0c96eabd9d0954e4b06ff07ecdcd27645c9a2504
phil-lgr <phil@leger.work>
2021-09-07 11:28:35 -0400

    test(core-figma-extractor): add workflow along with integration test

---
+++ packages/core-figma-extractor/test/integration.test.ts
@@ -0,0 +1,43 @@
+import { spawnSync } from 'child_process';
+import { extract } from '../src/extract';
+import fs from 'fs';
+import path from 'path';
+
+describe(`${extract} command`, () => {
+  it('should produce the correct tokens from the figma reference file (https://www.figma.com/file/Hcc7o8UnlnL6UOaVDnBhpm/integration-test?node-id=2233%3A5341)', () => {
+    const cmd = spawnSync(`yarn nr figma-sync`, {
+      cwd: process.cwd(),
+      shell: true,
+      stdio: 'inherit',
+      env: process.env,
+    });
+
+    const figmaColorJsonFile = fs.readFileSync(
+      path.resolve(process.cwd(), 'figma-export', 'figma-color.json'),
+      { encoding: 'utf8' }
+    );
+
+    expect(figmaColorJsonFile).toMatchSnapshot();
+
+    const figmaColorCssFile = fs.readFileSync(
+      path.resolve(process.cwd(), 'figma-export', 'figma-color.css'),
+      { encoding: 'utf8' }
+    );
+
+    expect(figmaColorCssFile).toMatchSnapshot();
+
+    const figmaColorScssFile = fs.readFileSync(
+      path.resolve(process.cwd(), 'figma-export', 'figma-color.scss'),
+      { encoding: 'utf8' }
+    );
+
+    expect(figmaColorScssFile).toMatchSnapshot();
+
+    const figmaColorTsFile = fs.readFileSync(
+      path.resolve(process.cwd(), 'figma-export', 'figma-color.ts'),
+      { encoding: 'utf8' }
+    );
+
+    expect(figmaColorTsFile).toMatchSnapshot();
+  });
+});
