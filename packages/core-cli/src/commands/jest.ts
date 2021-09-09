52cc45b6fd5fcb1b952e3cd8f7eef76a07cf1dd0
phil-lgr <phil@leger.work>
2021-09-09 14:05:56 -0400

    fix(core-cli): fix wrong jest command file

--- packages/core-cli/src/commands/jest.ts
+++ packages/core-cli/src/commands/jest.ts
@@ -1,67 +1,42 @@
 import { Command, flags } from '@oclif/command';
+import { spawnSync } from 'child_process';
 import debug from 'debug';
 import { NS } from '../utilities/log.utilities';
-import * as t from 'io-ts';
-import { loadDotEnv } from '@newrade/core-utils';
-import { spawnSync } from 'child_process';
 
-export type ENV = t.TypeOf<typeof Env>;
-export const Env = t.intersection([
-  t.type({}),
-  t.type({
-    VERCEL_PROJECT_ID: t.string,
-    VERCEL_ORG_ID: t.string,
-    VERCEL_TOKEN: t.string,
-    VERCEL_SCOPE: t.string,
-  }),
-]);
+export default class Jest extends Command {
+  log = debug(`${NS}:jest`);
+  logWarn = debug(`${NS}:jest:warn`);
+  logError = debug(`${NS}:jest:error`);
 
-export default class VercelDeploy extends Command {
-  log = debug(`${NS}:vercel-deploy`);
-  logWarn = debug(`${NS}:vercel-deploy:warn`);
-  logError = debug(`${NS}:vercel-deploy:error`);
+  static description = 'Shortcut to run jest with typescript (ts-node)';
 
-  static description = 'deploy site with vercel using env variables';
+  static examples = [`$ nr jest`];
 
-  static examples = [`$ nr vercel-deploy`];
+  static args = [{ name: 'args' }];
 
   static flags = {
-    help: flags.help({ char: 'h' }),
+    config: flags.string({ description: 'path to jest config file', default: 'jest.config.ts' }),
   };
 
-  static args = [];
-
-  async init() {}
-
   async run() {
-    this.log(`running in ${process.cwd()}`);
-    this.log('validating env variables');
-    this.log('loading .env file');
+    const { args, flags } = this.parse(Jest);
 
-    const env = loadDotEnv<ENV>({
-      schema: Env,
-      dotEnvPath: '.env',
-      dotEnvRootPath: '../../.env',
-      packageName: 'core-cli',
+    const command = [
+      `TS_NODE_PROJECT=../../tsconfig.node-commonjs.json node -r ts-node/register ../../node_modules/jest/bin/jest`,
+      `${flags.config ? '--config ' + flags.config : ''}`,
+      `${args.args || ''}`,
+    ].join(' ');
+
+    this.log(`running: ${command}`);
+
+    const cmd = spawnSync(command, {
+      shell: true,
+      stdio: ['inherit', 'inherit', 'pipe'],
+      env: process.env,
     });
-
-    this.log('running vercel deploy');
-
-    // use yarn to use the locally installed vercel-cli
-    const cmd = spawnSync(
-      `yarn vercel public --token $VERCEL_TOKEN --scope $VERCEL_SCOPE --confirm`,
-      {
-        cwd: '.',
-        shell: true,
-        stdio: ['inherit', 'inherit', 'pipe'],
-        env: env,
-      }
-    );
 
     if (cmd.stderr) {
       throw new Error(cmd.stderr.toString());
     }
-
-    this.log('done! ✅');
   }
 }
