b5f34d99fb99cf755ec28d046910a0bb700f4b52
phil-lgr <phil@leger.work>
2021-12-20 16:33:16 -0500

    fix(core): fix bad import

--- packages/ze-design-system/src/css/sizing.css.ts
+++ packages/ze-design-system/src/css/sizing.css.ts
@@ -1,7 +1,8 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
 import { sizingVars } from '@newrade/core-react-ui/theme';
-import { lightCssThemeV2 } from '@newrade/ze-design-system/src/design-system/theme';
+
+import { lightCssThemeV2 } from '../design-system/theme';
 
 // @ts-expect-error
 createGlobalTheme(':root', sizingVars, lightCssThemeV2.sizing);
--- packages/ze-design-system/src/css/typography.css.ts
+++ packages/ze-design-system/src/css/typography.css.ts
@@ -1,7 +1,8 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
 import { typographyVars } from '@newrade/core-react-ui/theme';
-import { lightCssThemeV2 } from '@newrade/ze-design-system/src/design-system/theme';
+
+import { lightCssThemeV2 } from '../design-system/theme';
 
 // @ts-expect-error
 createGlobalTheme(':root', typographyVars, lightCssThemeV2.typography);
